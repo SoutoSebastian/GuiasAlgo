3c6ce9542c6a5a9c61bf3ee38b281f6f50b4c4e6
soutosebastianma@gmail.com <soutosebastianma@gmail.com>
2023-09-04 13:34:53 -0300

    clase terminada

--- PracticaSebi/practica4.hs
+++ PracticaSebi/practica4.hs
@@ -10,5 +10,35 @@ parteEntera x | 0<=x && x<1 = 0
               | x<0 = -1 + parteEntera (x+1)
               | otherwise = 1 + parteEntera (x-1)
 
+---------------EJ 3------------------
+esDivisible :: Integer -> Integer -> Bool
+esDivisible x y | x == y = True
+                | x<y = False
+                | otherwise = esDivisible (x-y) y
+
 ---------------EJ 7----------------
-todosDigitosIguales
+
+todosDigitosIguales :: Integer -> Bool
+todosDigitosIguales n | n<10 = True
+                      | otherwise= primerDigito n == segundoDigito n && todosDigitosIguales (sacarPrimerDigito n)
+
+--funciones auxiliares
+
+primerDigito :: Integer -> Integer
+primerDigito n = mod n 10
+
+segundoDigito :: Integer -> Integer
+segundoDigito n = div (mod n 100) 10
+
+sacarPrimerDigito :: Integer -> Integer
+sacarPrimerDigito n = div n 10
+
+--------------EJ 8------------------
+iesimoDigito :: Integer -> Integer -> Integer
+iesimoDigito n i | i == cantDigitos n = primerDigito n
+                 | otherwise = iesimoDigito (sacarPrimerDigito n) i
+
+
+cantDigitos :: Integer -> Integer
+cantDigitos n | n<10 = 1
+              | otherwise = 1 + cantDigitos (div n 10)
