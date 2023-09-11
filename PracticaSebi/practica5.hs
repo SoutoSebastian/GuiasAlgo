5288748e870206c1f1f70b429f524573b78e65bd
Sebastian <soutosebastianma@gmail.com>
2023-09-11 18:19:34 -0300

    11/9 practica 5

--- PracticaSebi/practica5.hs
+++ PracticaSebi/practica5.hs
@@ -14,7 +14,68 @@ longitud :: [t] -> Integer
 longitud [] = 0
 longitud (_:xs) = 1 + longitud xs
 
+--2
+{--
+problema ultimo (s: seq⟨T⟩) : T {
+requiere: { |s| > 0 }
+asegura: { resultado = s[|s| − 1] }
+}
+--}
+
+ultimo :: [t] -> t 
+ultimo [x] = x
+ultimo (x:xs) = ultimo xs
+
+--3
+{--
+problema principio (s: seq⟨T⟩) : seq⟨T⟩ {
+requiere: { |s| > 0 }
+asegura: { resultado = subseq(s, 0, |s| − 1) }
+}
+--}
+
+principio :: [t] -> [t]
+principio [x] = []
+principio (x:xs) = x : principio xs
+
+--4
+{--
+problema reverso (s: seq⟨T⟩) : seq⟨T⟩ {
+requiere: { T rue }
+asegura: { resultado tiene los mismos elementos que s pero en orden inverso.}
+}
+--}
+
+reverso :: [t] -> [t]
+reverso [x] = [x]
+reverso (x:xs) = reverso xs ++ [x]
+
 ------EJ 2--------
+
+--1
+{--
+problema pertenece (e: T, s: seq⟨T⟩) : B {
+requiere: { T rue }
+asegura: { resultado = true ↔ e ∈ s }
+}
+--}
+
+pertenece :: (Eq t) => t -> [t] -> Bool
+pertenece _ [] = False
+pertenece n (x:xs)  | n==x = True
+                    | otherwise = pertenece n xs
+
+--2
+todosIguales :: (Eq t) => [t] -> Bool
+todosIguales [x] = True
+todosIguales (x:y:xs) | x == y = todosIguales (y:xs)
+                      | x /= y = False
+
+--3
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [x] = True
+todosDistintos (x:y:xs) | x /= y = todosDistintos (x:xs) && todosDistintos (y:xs)
+                        | x == y = False
 
 --4
 
